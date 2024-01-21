7ac272e80cccb05726ae21c493f85dbaece60b10
lion92 <38532569+lion92@users.noreply.github.com>
2024-01-21 10:56:50 +0100

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from "cookie-parser";
+import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{
-    logger: console
+  const app = await NestFactory.create(AppModule, {
+    logger: console,
   });
   app.use(cookieParser());
   app.enableCors({
     allowedHeaders: '*',
     origin: '*',
-    credentials:true
+    credentials: true,
   });
-  await console.log("ok")
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document)
+  await console.log('ok');
+  await app.listen(3004);
 }
 bootstrap();
